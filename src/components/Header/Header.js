c039b05acd33a1f393002a46af52ff1c95cbdd78
Krzysztof Suwaj <krzysztofsuwaj5@gmail.com>
2020-10-15 10:12:14 +0200

    Fix user showing in view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,16 @@
-import React, { useContext } from 'react'
-import User from '../User'
+import React  from 'react'
 import './header.css'
 
-import { UserContext } from '../../contexts/UserContextProvider'
 import Menu from '../Menu/Menu'
 
 const Header = () => {
 
-  const {id, token, username} = useContext(UserContext)
 
   return (
 
     <div className='header'>
       <div className='header__logo'>Coffee Ratings</div>
         <Menu/>
-         <User id={id} token={token} username={username}/>
     </div>
 
   )
